84a3974bf97cd13822162718d076e392812bed03
bqtruong <bqtruong@cox.net>
2013-12-11 16:00:11 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,1 +1,53 @@
 #include "main.h"
+
+robot_class::robot_class()
+{
+    
+}
+
+void robot_class::RobotInit()
+{
+    
+}
+
+void robot_class::DisabledInit()
+{
+    
+}
+
+void robot_class::AutonomousInit()
+{
+    
+}
+
+void robot_class::TeleopInit()
+{
+    
+}
+
+void robot_class::TestInit()
+{
+    
+}
+
+void robot_class::DisabledPeriodic()
+{
+    
+}
+
+void robot_class::AutonomousPeriodic()
+{
+    
+}
+
+void robot_class::TeleopPeriodic()
+{
+
+}
+
+void robot_class::TestPeriodic()
+{
+    
+}
+
+START_ROBOT_CLASS(robot_class);
