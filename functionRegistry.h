6dddff1b376bd80a2de7d9c1aaac2463c5c6f422
AlonBendelac <alon.bendelac@gmail.com>
2013-12-11 15:21:06 -0500

    Create functionRegistry.h
    created constructor and destructor

---
+++ functionRegistry.h
@@ -0,0 +1,12 @@
+#ifndef FUNCTIONREGISTRY_H_INCLUDED
+#define FUNCTIONREGISTRY_H_INCLUDED
+
+class functionRegistry {
+public:
+  void functionRegistry();
+  ~void functionRegistry();
+private:
+  
+};
+
+#endif // FUNCTIONREGISTRY_H_INCLUDED
