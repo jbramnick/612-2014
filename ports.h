cda5e55dd3cc8b0badf99d6102de539aba3bb30c
DanielBelsky <danfbel@gmail.com>
2014-02-01 13:43:34 -0500

    added some correct ports and module numbers in ports.h

--- ports.h
+++ ports.h
@@ -3,15 +3,15 @@
 
 
 // DriveTrain Channels
-const static uint32_t TALON_FL_CHANNEL = 0;
-const static uint32_t TALON_FR_CHANNEL = 1;
-const static uint32_t TALON_RL_CHANNEL = 3;
-const static uint32_t TALON_RR_CHANNEL = 4;
+const static uint32_t TALON_FL_CHANNEL = 1;
+const static uint32_t TALON_FR_CHANNEL = 6;
+const static uint32_t TALON_RL_CHANNEL = 2;
+const static uint32_t TALON_RR_CHANNEL = 7;
 // DriveTrain Modules
-const static uint8_t  TALON_FL_MODULE  = 0;
+const static uint8_t  TALON_FL_MODULE  = 1;
 const static uint8_t  TALON_FR_MODULE  = 1;
-const static uint8_t  TALON_RL_MODULE  = 3;
-const static uint8_t  TALON_RR_MODULE  = 4;
+const static uint8_t  TALON_RL_MODULE  = 1;
+const static uint8_t  TALON_RR_MODULE  = 1;
 // Pneumatics Channels
 const static uint32_t PNUM_DIGIN_CHANNEL = 1;
 const static uint32_t PNUM_RELAY_CHANNEL = 0;
@@ -20,16 +20,16 @@ const static uint8_t  PNUM_DIGIN_MODULE  = 1;
 const static uint8_t  PNUM_RELAY_MODULE  = 0;
 
 // Shifter
-const static uint32_t SHIFT_FCHAN = 0;
-const static uint32_t SHIFT_RCHAN = 1;
+const static uint32_t SHIFT_FCHAN = 7;
+const static uint32_t SHIFT_RCHAN = 8;
 const static uint8_t  SHIFT_MOD   = 1;
 
 // Shooter
 const static uint32_t SHOOT_TALON_CHANNEL = 1;
 const static uint8_t  SHOOT_TALON_MODULE  = 1;
 const static uint8_t  SHOOT_JAG_MODULE    = 1;
-const static uint32_t SHOOT_SLNOID_FCHAN  = 2;
-const static uint32_t SHOOT_SLNOID_RCHAN  = 3;
+const static uint32_t SHOOT_SLNOID_FCHAN  = 5;
+const static uint32_t SHOOT_SLNOID_RCHAN  = 6;
 const static uint8_t  SHOOT_SLNOID_MODULE = 1;
 
 // Encoder Modules
