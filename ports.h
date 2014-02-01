1bda157ec7378c1ecf429538decc954950e80b3a
anidev <anidev.aelico@gmail.com>
2014-02-01 14:49:16 -0500

    Fixed ports

--- ports.h
+++ ports.h
@@ -13,11 +13,11 @@ const static uint8_t  TALON_FR_MODULE  = 1;
 const static uint8_t  TALON_RL_MODULE  = 1;
 const static uint8_t  TALON_RR_MODULE  = 1;
 // Pneumatics Channels
-const static uint32_t PNUM_DIGIN_CHANNEL = 1;
-const static uint32_t PNUM_RELAY_CHANNEL = 0;
+const static uint32_t PNUM_DIGIN_CHANNEL = 8;
+const static uint32_t PNUM_RELAY_CHANNEL = 8;
 // Pneumatics Modules
 const static uint8_t  PNUM_DIGIN_MODULE  = 1;
-const static uint8_t  PNUM_RELAY_MODULE  = 0;
+const static uint8_t  PNUM_RELAY_MODULE  = 1;
 
 // Shifter
 const static uint32_t SHIFT_FCHAN = 7;
@@ -25,7 +25,7 @@ const static uint32_t SHIFT_RCHAN = 8;
 const static uint8_t  SHIFT_MOD   = 1;
 
 // Shooter
-const static uint32_t SHOOT_TALON_CHANNEL = 1;
+const static uint32_t SHOOT_TALON_CHANNEL = 3;
 const static uint8_t  SHOOT_TALON_MODULE  = 1;
 const static uint8_t  SHOOT_JAG_MODULE    = 1;
 const static uint32_t SHOOT_SLNOID_FCHAN  = 5;
@@ -34,14 +34,14 @@ const static uint8_t  SHOOT_SLNOID_MODULE = 1;
 
 // Encoder Modules
 const uint8_t  ENCODER_LMODULE_A  = 1;
-const uint8_t  ENCODER_RMODULE_A  = 2;
-const uint8_t  ENCODER_LMODULE_B  = 3;
-const uint8_t  ENCODER_RMODULE_B  = 4;
+const uint8_t  ENCODER_RMODULE_A  = 1;
+const uint8_t  ENCODER_LMODULE_B  = 1;
+const uint8_t  ENCODER_RMODULE_B  = 1;
 // Encoder Channels
-const uint32_t ENCODER_LCHANNEL_A = 5;
-const uint32_t ENCODER_RCHANNEL_A = 6;
-const uint32_t ENCODER_LCHANNEL_B = 7;
-const uint32_t ENCODER_RCHANNEL_B = 8;
+const uint32_t ENCODER_LCHANNEL_A = 1;
+const uint32_t ENCODER_RCHANNEL_A = 2;
+const uint32_t ENCODER_LCHANNEL_B = 3;
+const uint32_t ENCODER_RCHANNEL_B = 4;
 
 // Joystick
 const uint32_t DRIVER_JOY_PORT = 1;
